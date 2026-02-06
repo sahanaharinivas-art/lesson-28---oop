465b8e12014bada23885fa09b0e05f87aa343eb1
harinivassahana-max <harinivassahana@gmail.com>
2026-02-06 16:25:47 +1100

    activity 2

---
+++ activity 2.py
@@ -0,0 +1,17 @@
+class Employee:
+
+    def __init__(self):
+        print('employee created')
+
+    def __del__(self):
+        print("destructor called")
+
+def Create_obj():
+    print('making object...')
+    obj = Employee()
+    print('funtion end...')
+    return obj
+
+print('Calling Create_obj() function...')
+obj = Create_obj()
+print('Program End...')
